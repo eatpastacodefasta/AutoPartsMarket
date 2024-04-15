4dc4a14c870bba8c430f6b100490368ab62fcfcc
Chris <Chris@DESKTOP-F3DG83S>
2024-04-16 00:55:22 +0300

    Add project files.

---
+++ Site.Data/Models/RetailStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Site.Data.Models;
+
+public class RetailStore
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Address { get; set; }
+}
---
+++ Site.Data/Models/Supply.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Site.Data.Models;
+
+public class Supply
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+}
